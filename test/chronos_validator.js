22b094fd25227be74f828b81094dc37ade99f604
Daniel Kmak <kuzirashi@interia.pl>
2018-07-11 15:56:21 +0200

    ProxyWallet owner test.

--- test/chronos_validator.js
+++ test/chronos_validator.js
@@ -2,10 +2,7 @@ const ChronosValidator = artifacts.require('./ChronosValidator.sol');
 const SchedulerMock = artifacts.require('./Test/SchedulerMock.sol');
 const EventEmitter = artifacts.require('./external/chronos/EventEmitter.sol');
 const ProxyWallet = artifacts.require('./ProxyWallet.sol');
-const ScheduledTransactionMock = artifacts.require('./Test/ScheduledTransactionMock.sol');
 const ethUtil = require('ethereumjs-util');
-const Web3 = require('web3');
-const web3 = new Web3('http://localhost:8545');
 const stripHexPrefix = require('strip-hex-prefix');
 const abi = require('ethereumjs-abi');
 
@@ -20,8 +17,6 @@ const getTxRequestFromReceipt = (receipt) => {
   const log = receipt.logs.find(log => log.topics[0] === Constants.NEWREQUESTLOG)
   return "0x".concat(log.data.slice(26, 66));
 }
-
-const UINT256_0  = '0000000000000000000000000000000000000000000000000000000000000000';
 
 const SERIALIZED_TX_DATA = '600034603b57602f80600f833981f36000368180378080368173bebebebebebebebebebebebebebebebebebebebe5af415602c573d81803e3d81f35b80fd';
 
@@ -74,7 +69,6 @@ contract('ChronosValidator', function(accounts) {
 
   it('should return true when ScheduledTransaction canExecute returns true and transaction is in execution window', async function() {
     const chronosValidator = await ChronosValidator.deployed();
-
 
     const signerAddress = TEST_ACCOUNT_ADDRESS;
 
@@ -184,4 +178,48 @@ contract('ChronosValidator', function(accounts) {
 
     assert.strictEqual(recovered, signerAddress);
   });
+
+  it('should return false when message signer is not ScheduledTransaction owner', async function() {
+    const chronosValidator = await ChronosValidator.deployed();
+
+    const signerAddress = TEST_ACCOUNT_ADDRESS;
+
+    const eventEmitter = await EventEmitter.new();
+    const scheduler = await SchedulerMock.new(eventEmitter.address);
+
+    const proxyWallet = await ProxyWallet.new(scheduler.address, {
+      from: accounts[1]
+    });
+
+    const scheduleTransactionTx = await proxyWallet.schedule('0x01', {
+      from: accounts[1]
+    });
+    const scheduledTransactionAddress = getTxRequestFromReceipt(scheduleTransactionTx.receipt)
+
+    const signerPrivateKey = ethUtil.toBuffer(TEST_PRIVATE_KEY);
+
+    const ecSignature = ethUtil.ecsign(
+      ethUtil.hashPersonalMessage(ethUtil.toBuffer(scheduledTransactionAddress + stripHexPrefix(TEST_ORDER_HASH))),
+      signerPrivateKey
+    );
+
+    // Create 0x signature from EthSign signature
+    let signedScheduledTxAddress = stripHexPrefix(ethUtil.bufferToHex(Buffer.concat([
+        ethUtil.toBuffer(ecSignature.v),
+        ecSignature.r,
+        ecSignature.s
+    ])));
+
+    const serializedScheduledTxDataByteLength = getBytesLengthFromHexString(SERIALIZED_TX_DATA);
+
+    const serializedLength = stripHexPrefix(ethUtil.bufferToHex(abi.rawEncode([ 'uint256' ], [ serializedScheduledTxDataByteLength ])));
+
+    const signature = scheduledTransactionAddress + serializedLength + SERIALIZED_TX_DATA
+      + signedScheduledTxAddress;
+
+    const isValid = await chronosValidator.isValidSignature.call(TEST_ORDER_HASH, signerAddress, signature);
+
+    assert.isFalse(isValid);
+  });
+
 });
